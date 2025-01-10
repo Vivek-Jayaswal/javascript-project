bf092e1e0c539a4f1a7145bc07959a43b7c0eea5
Vivek-Jayaswal <vivekkumarjaiswal2000@gmail.com>
2025-01-10 11:16:14 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,143 @@
+let data = [
+    {
+        "name": "Veggie Delight",
+        "imageSrc": "https://source.unsplash.com/random?veggies",
+        "time": "30 min",
+        "type": "veg",
+        "isLiked": false,
+        "rating": 4.2
+    },
+    {
+        "name": "Chicken Grill",
+        "imageSrc": "https://source.unsplash.com/random?chicken",
+        "time": "45 min",
+        "type": "non-veg",
+        "isLiked": false,
+        "rating": 4.5
+    },
+    {
+        "name": "Cheese Pizza",
+        "imageSrc": "https://source.unsplash.com/random?pizza",
+        "time": "40 min",
+        "type": "veg",
+        "isLiked": false,
+        "rating": 4.1
+    },
+    {
+        "name": "Steak",
+        "imageSrc": "https://source.unsplash.com/random?steak",
+        "time": "60 min",
+        "type": "non-veg",
+        "isLiked": false,
+        "rating": 4.7
+    },
+    {
+        "name": "Grilled Salmon",
+        "imageSrc": "https://source.unsplash.com/random?salmon",
+        "time": "50 min",
+        "type": "non-veg",
+        "isLiked": false,
+        "rating": 4.6
+    },
+    {
+        "name": "Tomato Pasta",
+        "imageSrc": "https://source.unsplash.com/random?pasta",
+        "time": "35 min",
+        "type": "veg",
+        "isLiked": false,
+        "rating": 4.0
+    },
+    {
+        "name": "Vegan Salad",
+        "imageSrc": "https://source.unsplash.com/random?salad",
+        "time": "20 min",
+        "type": "veg",
+        "isLiked": false,
+        "rating": 3.9
+    },
+    {
+        "name": "Fried Chicken",
+        "imageSrc": "https://source.unsplash.com/random?friedChicken",
+        "time": "55 min",
+        "type": "non-veg",
+        "isLiked": false,
+        "rating": 4.3
+    },
+    {
+        "name": "Mushroom Risotto",
+        "imageSrc": "https://source.unsplash.com/random?risotto",
+        "time": "45 min",
+        "type": "veg",
+        "isLiked": false,
+        "rating": 4.5
+    },
+    {
+        "name": "Burger",
+        "imageSrc": "https://source.unsplash.com/random?burger",
+        "time": "30 min",
+        "type": "non-veg",
+        "isLiked": false,
+        "rating": 4.2
+    },
+    {
+        "name": "Paneer Tikka",
+        "imageSrc": "https://source.unsplash.com/random?paneerTikka",
+        "time": "40 min",
+        "type": "veg",
+        "isLiked": false,
+        "rating": 4.4
+    },
+    {
+        "name": "BBQ Ribs",
+        "imageSrc": "https://source.unsplash.com/random?ribs",
+        "time": "70 min",
+        "type": "non-veg",
+        "isLiked": false,
+        "rating": 4.6
+    },
+    {
+        "name": "Caesar Salad",
+        "imageSrc": "https://source.unsplash.com/random?caesarSalad",
+        "time": "25 min",
+        "type": "veg",
+        "isLiked": false,
+        "rating": 3.8
+    },
+    {
+        "name": "Fish Tacos",
+        "imageSrc": "https://source.unsplash.com/random?fishTacos",
+        "time": "35 min",
+        "type": "non-veg",
+        "isLiked": false,
+        "rating": 4.3
+    },
+    {
+        "name": "Chocolate Cake",
+        "imageSrc": "https://source.unsplash.com/random?chocolateCake",
+        "time": "90 min",
+        "type": "veg",
+        "isLiked": false,
+        "rating": 4.9
+    }
+]
+
+
+// You are tasked with creating an interactive recipe catalogue. The catalogue should allow users to view recipes from a predefined array and provide several interactive filtering and sorting options.
+// You will be provided with a JSON array of recipe objects. Each recipe object will have the following properties:
+
+// name: Name of the recipe
+// imageSrc: URL to the recipe's cover image
+// time: Time required to prepare the recipe (e.g., "40 min")
+// type: Whether the recipe is "veg" or "non-veg"
+// isLiked: Boolean that indicates whether the recipe is liked (default is false)
+// rating: A decimal number between 1 and 5 that represents the recipe's rating
+
+// Features
+// Display Recipes: Map over the array of recipes and create a UI card for each recipe. Each card should display the recipe's cover image, name, type, time, rating, and a "like" button (represented by a heart icon).
+// Search Recipes: Include a search bar that allows the user to filter recipes by name. As they type, dynamically update the displayed recipes to only include those whose names match the search query.
+// Toggle Recipe Type: Provide three toggle buttons: "Show All," "Show Only Veg," and "Show Only Non-Veg." When a button is clicked, the displayed recipes should be updated to only include recipes that match the selected type.
+// Filter by Rating: Include two radio buttons labelled "Rating Above 4.5" and "Rating Below 4.0." When a button is selected, the displayed recipes should be updated to only include recipes whose ratings match the selected filter.
+// Responsive Design: The website should be fully responsive and should display properly on both desktop and mobile devices. Make a drawer for the mobile screen and that drawer should be toggled using JS. As shown in the mobile screen of the figma.
+
+
+export {data}
---
+++ script.js
@@ -0,0 +1,174 @@
+let data = [
+  {
+    name: "Veggie Delight",
+    imageSrc: "https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?cs=srgb&dl=pexels-ella-olsson-572949-1640772.jpg&fm=jpg",
+    time: "30 min",
+    type: "veg",
+    isLiked: false,
+    rating: 4.2,
+  },
+  {
+    name: "Chicken Grill",
+    imageSrc: "https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?cs=srgb&dl=pexels-ella-olsson-572949-1640772.jpg&fm=jpg",
+    time: "45 min",
+    type: "non-veg",
+    isLiked: false,
+    rating: 4.5,
+  },
+  {
+    name: "Cheese Pizza",
+    imageSrc: "https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?cs=srgb&dl=pexels-ella-olsson-572949-1640772.jpg&fm=jpg",
+    time: "40 min",
+    type: "veg",
+    isLiked: false,
+    rating: 4.1,
+  },
+  {
+    name: "Steak",
+    imageSrc: "https://www.foodiesfeed.com/wp-content/uploads/2023/06/burger-with-melted-cheese.jpg",
+    time: "60 min",
+    type: "non-veg",
+    isLiked: false,
+    rating: 4.7,
+  },
+  {
+    name: "Grilled Salmon",
+    imageSrc: "https://www.foodiesfeed.com/wp-content/uploads/2023/06/burger-with-melted-cheese.jpg",
+    time: "50 min",
+    type: "non-veg",
+    isLiked: false,
+    rating: 4.6,
+  },
+  {
+    name: "Tomato Pasta",
+    imageSrc: "https://www.foodiesfeed.com/wp-content/uploads/2023/06/burger-with-melted-cheese.jpg",
+    time: "35 min",
+    type: "veg",
+    isLiked: false,
+    rating: 4.0,
+  },
+  {
+    name: "Vegan Salad",
+    imageSrc: "https://www.foodiesfeed.com/wp-content/uploads/2023/06/burger-with-melted-cheese.jpg",
+    time: "20 min",
+    type: "veg",
+    isLiked: false,
+    rating: 3.9,
+  },
+  {
+    name: "Fried Chicken",
+    imageSrc: "https://www.foodiesfeed.com/wp-content/uploads/2023/06/burger-with-melted-cheese.jpg",
+    time: "55 min",
+    type: "non-veg",
+    isLiked: false,
+    rating: 4.3,
+  },
+  {
+    name: "Mushroom Risotto",
+    imageSrc: "https://www.foodiesfeed.com/wp-content/uploads/2023/06/burger-with-melted-cheese.jpg",
+    time: "45 min",
+    type: "veg",
+    isLiked: false,
+    rating: 4.5,
+  },
+  {
+    name: "Burger",
+    imageSrc: "https://www.foodiesfeed.com/wp-content/uploads/2023/06/burger-with-melted-cheese.jpg",
+    time: "30 min",
+    type: "non-veg",
+    isLiked: false,
+    rating: 4.2,
+  },
+  {
+    name: "Paneer Tikka",
+    imageSrc: "https://www.foodiesfeed.com/wp-content/uploads/2023/06/burger-with-melted-cheese.jpg",
+    time: "40 min",
+    type: "veg",
+    isLiked: false,
+    rating: 4.4,
+  },
+  {
+    name: "BBQ Ribs",
+    imageSrc: "https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?cs=srgb&dl=pexels-ella-olsson-572949-1640772.jpg&fm=jpg",
+    time: "70 min",
+    type: "non-veg",
+    isLiked: false,
+    rating: 4.6,
+  },
+  {
+    name: "Caesar Salad",
+    imageSrc: "https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?cs=srgb&dl=pexels-ella-olsson-572949-1640772.jpg&fm=jpg",
+    time: "25 min",
+    type: "veg",
+    isLiked: false,
+    rating: 3.8,
+  },
+  {
+    name: "Fish Tacos",
+    imageSrc: "https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?cs=srgb&dl=pexels-ella-olsson-572949-1640772.jpg&fm=jpg",
+    time: "35 min",
+    type: "non-veg",
+    isLiked: false,
+    rating: 4.3,
+  },
+  {
+    name: "Chocolate Cake",
+    imageSrc: "https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?cs=srgb&dl=pexels-ella-olsson-572949-1640772.jpg&fm=jpg",
+    time: "90 min",
+    type: "veg",
+    isLiked: false,
+    rating: 4.9,
+  },
+];
+
+const cardContainer = document.querySelector(
+  "#menu-section>#recipe-container>#card-container"
+);
+
+function card() {
+  data.forEach((item) => {
+    const cardDiv = document.createElement("div");
+    cardDiv.className = "card"
+    const image = document.createElement("img");
+    image.src = item.imageSrc;
+    image.alt = "card image",
+    image.className = "image"
+
+    const paragrph = document.createElement("p");
+    paragrph.innerText = item.type;
+    paragrph.className = "paragraph";
+
+    const divnamerating = document.createElement("div");
+    divnamerating.className="name-rating";
+    const foodnamepara = document.createElement("p")
+    foodnamepara.innerText = item.name;
+    foodnamepara.className = "title";
+    const rating = document.createElement("span");
+    rating.innerText = item.rating;
+    rating.className = "rating"
+    divnamerating.append(foodnamepara,rating);
+
+    const timedivlikemessage = document.createElement("div");
+    timedivlikemessage.className = "like-message-time"
+    const timepara = document.createElement("p");
+    timepara.textContent = item.time;
+    timepara.className = "time";
+    const messagelikediv = document.createElement("div");
+    messagelikediv.className = "like-message";
+    const messageSpan = document.createElement("span");
+    messageSpan.className = "material-icons message";
+    messageSpan.innerText = "sms"
+    // messageSpan.className = "message";
+    const likeSpan = document.createElement("span");
+    likeSpan.className = "material-icons like";
+    likeSpan.innerText = "favorite"
+    // likeSpan.className = "like"
+    messagelikediv.append(messageSpan,likeSpan);
+    timedivlikemessage.append(timepara,messagelikediv)
+
+    cardDiv.append(image,paragrph,divnamerating,timedivlikemessage);
+    cardContainer.appendChild(cardDiv);
+  });
+}
+
+card();
